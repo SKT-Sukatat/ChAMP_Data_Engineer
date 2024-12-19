a636e1cd5b8610f6b4d2432a448f3f894f6b70f4
Sukatat <skt.sukatat@gmail.com>
2024-12-19 17:20:23 +0700

    Update Pydantic

--- Workshop_Traffy/traffy_data_pipeline_solutions.py
+++ Workshop_Traffy/traffy_data_pipeline_solutions.py
@@ -1,5 +1,7 @@
+from pydantic import BaseModel
 from airflow.decorators import dag, task
 from airflow.utils.dates import days_ago
+from airflow.exceptions import AirflowFailException
 
 import pandas as pd
 from datetime import datetime, timedelta
@@ -17,9 +19,29 @@ default_args = {
     'schedule_interval':'0 8 * * *'
 }
 
+
 # Define the today date
 today_date = datetime.now().strftime("%d_%m_%Y")
 
+# Define a Pydantic model for your schema
+class TraffySchema(BaseModel):
+    ticket_id: object
+    type: object
+    organization: object
+    comment: object
+    photo: object
+    photo_after: object
+    coords: object
+    address: object
+    subdistrict: object
+    district: object
+    province: object
+    timestamp: object
+    state: object
+    star: float
+    count_reopen: int
+    last_activity: object
+            
 
 @task()
 def save_raw_data(output_path):
@@ -28,6 +50,25 @@ def save_raw_data(output_path):
     parquet_filename = "traffy_raw_data_" + today_date + ".parquet"
     # Export the DataFrame to a Parquet file
     df_traffy_raw.to_parquet(output_path + parquet_filename, engine="pyarrow", index=False)
+
+
+@task()
+def validate_columns(input_path):
+    # Load the Parquet file (replace with your actual file path or file source)
+    parquet_filename = "traffy_raw_data_" + today_date + ".parquet"
+    df = pd.read_parquet(input_path + parquet_filename)
+
+    # Get required columns from the Pydantic model
+    required_columns = set(TraffySchema.__fields__.keys())
+    file_columns = set(df.columns)
+
+    # Check for missing or extra columns
+    missing_columns = required_columns - file_columns
+
+    if missing_columns:
+        raise AirflowFailException(f"Missing columns: {missing_columns}")
+    else:
+        print("All required columns are present.")
 
 
 @task()
@@ -66,8 +107,10 @@ def traffy_pipeline():
 
     print_load_success = print_success()
 
+    validate_columns_task = validate_columns(input_path= '/opt/airflow/dags/traffy_data/')
+
     # Crate Task Dependency (Create DAG)
-    save_raw_data_task >> etl_traffy_data_task >> print_load_success
+    save_raw_data_task >> validate_columns_task >> etl_traffy_data_task >> print_load_success
 
 
 traffy_pipeline()
