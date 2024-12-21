25dbeb225b28a6af7f81abf1166ee06550fbaa30
SKT-Sukatat <82657530+SKT-Sukatat@users.noreply.github.com>
2024-12-21 11:39:23 +0700

    Update task_flow_api_custom.py

--- Workshop_Traffy/task_flow_api_custom.py
+++ Workshop_Traffy/task_flow_api_custom.py
@@ -29,6 +29,62 @@ from airflow.operators.dummy import DummyOperator
 
 # [END import_module]
 
+# [START extract]
+@task()
+def extract():
+    """
+    #### Extract task
+    A simple Extract task to get data ready for the rest of the data
+    pipeline. In this case, getting data is simulated by reading from a
+    hardcoded JSON string.
+    """
+    data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
+
+    order_data_dict = json.loads(data_string)
+    return order_data_dict
+
+# [END extract]
+
+# [START transform]
+@task(multiple_outputs=True)
+def transform(order_data_dict: dict):
+    """
+    #### Transform task
+    A simple Transform task which takes in the collection of order data and
+    computes the total order value.
+    """
+    total_order_value = 0
+
+    for value in order_data_dict.values():
+        total_order_value += value
+
+    return {"total_order_value": total_order_value}
+
+# [END transform]
+
+# [START load]
+@task()
+def load(total_order_value: float):
+    """
+    #### Load task
+    A simple Load task which takes in the result of the Transform task and
+    instead of saving it to end user review, just prints it out.
+    """
+
+    print(f"Total order value is: {total_order_value:.2f}")
+
+# [END load]
+
+# [START main_flow]
+# order_data = extract()
+# order_summary = transform(order_data)
+# load(order_summary["total_order_value"])
+# [END main_flow]
+
+
+@task()
+def print_finished():
+    print("SUCCESS: All task finished")
 
 # [START instantiate_dag]
 @dag(
@@ -46,64 +102,7 @@ def tutorial_taskflow_api_custom():
     located
     [here](https://airflow.apache.org/docs/apache-airflow/stable/tutorial_taskflow_api.html)
     """
-    # [END instantiate_dag]
 
-    # [START extract]
-    @task()
-    def extract():
-        """
-        #### Extract task
-        A simple Extract task to get data ready for the rest of the data
-        pipeline. In this case, getting data is simulated by reading from a
-        hardcoded JSON string.
-        """
-        data_string = '{"1001": 301.27, "1002": 433.21, "1003": 502.22}'
-
-        order_data_dict = json.loads(data_string)
-        return order_data_dict
-
-    # [END extract]
-
-    # [START transform]
-    @task(multiple_outputs=True)
-    def transform(order_data_dict: dict):
-        """
-        #### Transform task
-        A simple Transform task which takes in the collection of order data and
-        computes the total order value.
-        """
-        total_order_value = 0
-
-        for value in order_data_dict.values():
-            total_order_value += value
-
-        return {"total_order_value": total_order_value}
-
-    # [END transform]
-
-    # [START load]
-    @task()
-    def load(total_order_value: float):
-        """
-        #### Load task
-        A simple Load task which takes in the result of the Transform task and
-        instead of saving it to end user review, just prints it out.
-        """
-
-        print(f"Total order value is: {total_order_value:.2f}")
-
-    # [END load]
-
-    # [START main_flow]
-    # order_data = extract()
-    # order_summary = transform(order_data)
-    # load(order_summary["total_order_value"])
-    # [END main_flow]
-
-
-    @task()
-    def print_finished():
-        print("SUCCESS: All task finished")
 
     # Declare task variables
     extract_task = extract()
@@ -112,6 +111,8 @@ def tutorial_taskflow_api_custom():
     dummy_operator_task = DummyOperator(task_id='dummy_task')
     print_finished_task = print_finished()
 
+
+    # Declare DAG dependency
     extract_task >> transform_task >> load_task >> dummy_operator_task >> print_finished_task
 
     
